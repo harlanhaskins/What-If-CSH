b3b78a49e43f89b6caa00c401aa3e507598c9c26
Harlan Haskins <harlan@harlanhaskins.com>
2015-05-08 00:17:28 -0400

    Fixed frontend upvote display

--- Frontend/js/app.js
+++ Frontend/js/app.js
@@ -1,7 +1,7 @@
 angular.module('whatIfCSH', [])
-  .controller('WhatIfController', function($scope, $http) {
+.controller('WhatIfController', function($scope, $http) {
     var base = 'https://whatif.csh.rit.edu/api'
-    $scope.suggestions = [];
+        $scope.suggestions = [];
     $scope.description = "";
     $scope.error = null;
 
@@ -10,29 +10,35 @@ angular.module('whatIfCSH', [])
     }
 
     $http.get(base + '/suggestions')
-         .success(function(data, status, headers, config) {
-             $scope.suggestions = data.suggestions;
-             $scope.user = data.user;
-         })
-         .error(errorCallback);
+        .success(function(data, status, headers, config) {
+            data.suggestions.forEach(function(suggestion, _, _) {
+                if (suggestion.vote) {
+                    console.log(suggestion.score);
+                    suggestion.score -= suggestion.vote;
+                }
+            });
+            $scope.suggestions = data.suggestions;
+            $scope.user = data.user;
+        })
+    .error(errorCallback);
 
     $scope.score = function(suggestion) {
-        var score = suggestion.score + (suggestion.vote === null ? 0 : suggestion.vote);
+        var score = suggestion.score + (suggestion.vote == null ? 0 : suggestion.vote)
         return score + " point" + (score == 1 ? '' : 's');
     }
 
     $scope.vote = function(suggestion, upvote) {
         if ((suggestion.vote == 1 && upvote) || (suggestion.vote == -1 && !upvote)) {
             $http.delete(base + '/suggestions/' + suggestion.id + '/vote')
-                 .success(function(data, status, headers, config) {
-                     suggestion.vote = 0;
-                 });
+                .success(function(data, status, headers, config) {
+                    suggestion.vote = null;
+                });
             return;
         }
-        $http.put(base + '/suggestions/' + suggestion.id + '/vote/' + (upvote ? 1 : -1))
-             .success(function(data, status, headers, config) {
-                 suggestion.vote = upvote ? 1 : -1;
-             });
+        $http.put(base + '/suggestions/' + suggestion.id + '/vote/' + (upvote ? 'up' : 'down'))
+            .success(function(data, status, headers, config) {
+                suggestion.vote = upvote ? 1 : -1;
+            });
     };
 
     $scope.upvoteClass = function(suggestion) {
@@ -55,26 +61,25 @@ angular.module('whatIfCSH', [])
         var trimmed = $scope.description.trim();
         if (!trimmed) return;
         $http.post(base + '/suggestions', {description: trimmed})
-             .success(function(data, status, headers, config) {
-               $scope.description = "";
-               console.log(data);
-               $scope.suggestions.splice(0, 0, data);
-             })
-             .error(errorCallback);
+            .success(function(data, status, headers, config) {
+                $scope.description = "";
+                $scope.suggestions.splice(0, 0, data);
+            })
+        .error(errorCallback);
     };
 
     $scope.delete = function(suggestion) {
         $http.delete(base + "/suggestions/" + suggestion.id)
-             .success(function(data, status, headers, config) {
-               var index = $scope.suggestions.indexOf(suggestion);
-               if (index > -1) {
-                 $scope.suggestions.splice(index, 1);
-               }
-             })
-             .error(errorCallback);
+            .success(function(data, status, headers, config) {
+                var index = $scope.suggestions.indexOf(suggestion);
+                if (index > -1) {
+                    $scope.suggestions.splice(index, 1);
+                }
+            })
+        .error(errorCallback);
     }
     $scope.timestamp = function(suggestion) {
-       var m = moment(suggestion.timestamp);
-       return m.fromNow()
+        var m = moment(suggestion.timestamp);
+        return m.fromNow()
     }
 });
